32bef5128fe50fafa6115a82754706e87c8c619b
john.agudelo <johnaagudelo@gmail.com>
2023-07-07 15:27:39 -0500

    ✨ get products

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({ title, price, categoryName, image }) => {
   return (
     <div className="bg-white cursor-pointer w-56 h-60 rounded-lg">
       <figure className="relative mb-2 w-full h-4/5">
         <span className="absolute bottom-2 left-2 bg-white/60 rounded-lg text-black text-xs px-3 py-0.5 shadow-black">
-          Electronics
+          {categoryName}
         </span>
         <img
           className="w-full h-full object-cover rounded-lg"
-          src="https://images.pexels.com/photos/1649771/pexels-photo-1649771.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          src={image}
           alt="headphone"
         />
         <div className="absolute top-1 right-1 flex justify-center items-center bg-white w-5 h-5 rounded-full">
@@ -17,8 +17,8 @@ const Card = () => {
         </div>
       </figure>
       <p className="flex justify-between">
-        <span className="text-sm font-light">Headphones</span>
-        <span className="text-lg font-medium">$300</span>
+        <span className="text-sm font-light">{title}</span>
+        <span className="text-lg font-medium">{`$ ${price}`}</span>
       </p>
     </div>
   );
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,47 @@
+import { useEffect, useState } from 'react';
 import Card from '../../components/Card';
 import Layout from '../../components/Layout';
 
+const API = 'https://api.escuelajs.co/api/v1';
+
 function Home() {
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const request = await fetch(`${API}/products`);
+        if (request.ok) {
+          const data = await request.json();
+          setProducts(data);
+          return;
+        }
+
+        setError('Error al cargar los productos');
+      } catch (error) {
+        setError('Error al cargar los productos');
+      }
+    };
+    getProducts();
+  }, []);
+
   return (
     <Layout>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      {error && <h3>{error}</h3>}
+      <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+        {products.map(
+          ({ id, title, category: { name }, price, images: [image] }) => (
+            <Card
+              key={id}
+              title={title}
+              categoryName={name}
+              price={price}
+              image={image}
+            />
+          )
+        )}
+      </div>
     </Layout>
   );
 }
